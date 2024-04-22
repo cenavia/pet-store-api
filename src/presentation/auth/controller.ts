789fb7a554c8a1daad88fa7dd9d5f6480a0a81b6
Carlos Navia <cenaviat@gmail.com>
2024-04-21 22:47:49 -0500

    Generación del DTO desde el body de la request register de la Api

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'; // 5
+import { RegisterUserDto } from './../../domain'; // 1
 
 
 
@@ -8,12 +9,14 @@ export class AuthController { // 1
     constructor(){} // 2
 
     registerUser = (req: Request, res: Response) => { // 4
-        res.json('registerUser controller') // 5
+        const [error, registerUserDto] = RegisterUserDto.create(req.body);
+        if ( error ) return res.status(400).json({ error });
+
+        res.json(registerUserDto)
     } // 3
 
     loginUser =  (req: Request, res: Response) => { // 7
         res.json('loginUser controller') // 8
-
     } // 6
 
 }
